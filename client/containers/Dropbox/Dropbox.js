1fdf4999a442156c6caa46cdae1f37dcc257cb7d
Julien <ju.collet@gmail.com>
2017-09-25 13:47:21 +0000

    Fixes #6

--- client/containers/Dropbox/Dropbox.js
+++ client/containers/Dropbox/Dropbox.js
@@ -30,16 +30,26 @@ class Dropbox extends Component {
     
     const callback = function(delay = 1000){
 
+      const iconEl = document.getElementById(`${id}-icon`);
+      const progressEl = document.getElementById(`${id}-progress`);
+      const dropZoneEl = document.getElementById(`${id}-dropzone`);
+
       if(this.props.redirection){
         this.props.history.push(redirection);
       }
-      document.getElementById(`${id}-icon`).classList.remove("fa-cog", "fa-spin");
-      document.getElementById(`${id}-icon`).classList.add("fa-check", "txt-white");
+      
+      if(iconEl){
+        iconEl.classList.remove("fa-cog", "fa-spin");
+        iconEl.classList.add("fa-check", "txt-white");
+      }
+
       setTimeout(_ => {
-        document.getElementById(`${id}-progress`).style.height = ("0px");
-        document.getElementById(`${id}-icon`).classList.remove("dropIconAnim", "fa-check", "txt-white");
-        document.getElementById(`${id}-dropzone`).classList.remove("dragUploadDragHover");
-        document.getElementById(`${id}-icon`).classList.add("fa-paper-plane");
+        if(progressEl && iconEl && dropZoneEl) {
+          progressEl.style.height = ("0px");
+          iconEl.classList.remove("dropIconAnim", "fa-check", "txt-white");
+          dropZoneEl.classList.remove("dragUploadDragHover");
+          iconEl.classList.add("fa-paper-plane");  
+        }
       }, delay);
     }.bind(this);
 
@@ -62,9 +72,7 @@ class Dropbox extends Component {
 
     return(
         <div id={`${this.props.id}-dropzone`} style={{height:this.props.height}} className="dropzone" onDrop={e => this.onDrop(e)} onDragOver={e => this.onDragOver(e)} onDragLeave={e => this.onDragLeave(e)} >
-          <div className="dropLimits">
             <i id={`${this.props.id}-icon`} className="fa fa-paper-plane"></i>
-          </div>
           <div className="dropzone-progress" id={`${this.props.id}-progress`} >
           </div>
         </div>
