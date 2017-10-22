4e4989b6b665f8b67e10d2404dac4d27d09a1637
Julien <ju.collet@gmail.com>
2017-10-22 14:06:33 +0000

    Add google Analytics script

--- client/containers/Account/MyUsage/MyUsage.js
+++ client/containers/Account/MyUsage/MyUsage.js
@@ -22,9 +22,6 @@ class MyUsage extends React.Component {
                 <VolumeGauge volume={infos.volume ? infos.volume : 0 } usedVolume={infos.usedVolume ? infos.usedVolume : 0}/>
                 <h3 className="margin-md-top">{infos.numberOfAlbums ? infos.numberOfAlbums : 0} album{infos.numberOfAlbums && infos.numberOfAlbums > 1 ? "s" : null}, {infos.numberOfPhotos ? infos.numberOfPhotos : 0} photo{infos.numberOfPhotos && infos.numberOfPhotos > 1 ? "s":null}</h3>
                 <p className="margin-md-bottom">Tu as {infos.numberOfPhotos ? infos.numberOfPhotos : 0} photo{infos.numberOfPhotos && infos.numberOfPhotos > 1 ? "s":null}, réparties dans {infos.numberOfAlbums ? infos.numberOfAlbums : 0} album{infos.numberOfAlbums && infos.numberOfAlbums > 1 ? "s" : null}.</p>
-                <hr/>
-                <h3 className="margin-md-top">Besoin de plus d'espace ?</h3>
-                <p>Achetez de l'espace supplémentaire pour sauvegarder plus de photos ! Heheheh c'est trop coooooool</p>
             </div>
         );
     }
